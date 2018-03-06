b6221b1813dc3fb3ed42f0bd75c9b75b911c7591
Dan Baumfeld <thethirddan@gmail.com>
2018-03-05 20:53:05 -0800

    skeleton

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
   schedule = [
-    {day: "Tuesday", time: "7p - 9", location:"Evolv Fitness"},
-    {day: "Thursday", time: "7p - 9", location:"Evolv Fitness"},
-    {day: "Sunday", time: "5p - 7", location:"Koei-Kan Academy"},
+    {day: "Every", time: "ALL", location:"Everywhere"}
   ];
 
   constructor() { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title = 'DuckJitsu';
-  heroImg = 'assets/img/yotd.jpg';
-  heroLink = 'http://www.happenstancecoffee.com/duck/';
+  title = '#CoffeeToo';
+  heroImg = 'assets/img/coffeetoologo.jpg';
+  heroLink = '#';
 
   constructor() { }
 
